da63f4d3f4564acbe458820312630f3669b3730f
Chris de Graaf <chrisadegraaf@gmail.com>
2019-02-11 01:45:39 -0600

    Rename a function

--- src/minifier.jl
+++ src/minifier.jl
@@ -36,59 +36,59 @@ end
 
 complete(ctx::Minifier) = ctx.io
 
-write(ctx::Minifier, v::Ptr{UInt8}, len::Int) = unsafe_write(ctx.io, v, len)
-write(ctx::Minifier, v::Vector{UInt8}, ::Int=-1) = Base.write(ctx.io, v)
-write(ctx::Minifier, v::UInt8, ::Int=-1) = Base.write(ctx.io, v)
+writeval(ctx::Minifier, v::Ptr{UInt8}, len::Int) = unsafe_write(ctx.io, v, len)
+writeval(ctx::Minifier, v::Vector{UInt8}, ::Int=-1) = write(ctx.io, v)
+writeval(ctx::Minifier, v::UInt8, ::Int=-1) = write(ctx.io, v)
 
 # Yuck.
 function addval(ctx::Minifier, v, len::Int=-1; string::Bool=false)
     state = last(ctx.state)
     if state === MINIFY_INIT
-        string && write(ctx, QUOTE)
-        write(ctx, v, len)
-        string && write(ctx, QUOTE)
+        string && writeval(ctx, QUOTE)
+        writeval(ctx, v, len)
+        string && writeval(ctx, QUOTE)
     elseif state === MINIFY_MAP_KEY
-        write(ctx, [COMMA, QUOTE])
-        write(ctx, v, len)
-        write(ctx, [QUOTE, COLON])
+        writeval(ctx, [COMMA, QUOTE])
+        writeval(ctx, v, len)
+        writeval(ctx, [QUOTE, COLON])
         ctx.state[lastindex(ctx.state)] = MINIFY_MAP_VAL
     elseif state === MINIFY_MAP_KEY_FIRST
-        write(ctx, QUOTE)
-        write(ctx, v, len)
-        write(ctx, [QUOTE, COLON])
+        writeval(ctx, QUOTE)
+        writeval(ctx, v, len)
+        writeval(ctx, [QUOTE, COLON])
         ctx.state[lastindex(ctx.state)] = MINIFY_MAP_VAL
     elseif state === MINIFY_MAP_VAL
-        string && write(ctx, QUOTE)
-        write(ctx, v, len)
-        string && write(ctx, QUOTE)
+        string && writeval(ctx, QUOTE)
+        writeval(ctx, v, len)
+        string && writeval(ctx, QUOTE)
         ctx.state[lastindex(ctx.state)] = MINIFY_MAP_KEY
     elseif state === MINIFY_ARRAY
-        write(ctx, COMMA)
-        string && write(ctx, QUOTE)
-        write(ctx, v, len)
-        string && write(ctx, QUOTE)
+        writeval(ctx, COMMA)
+        string && writeval(ctx, QUOTE)
+        writeval(ctx, v, len)
+        string && writeval(ctx, QUOTE)
     elseif state === MINIFY_ARRAY_FIRST
-        string && write(ctx, QUOTE)
-        write(ctx, v, len)
-        string && write(ctx, QUOTE)
+        string && writeval(ctx, QUOTE)
+        writeval(ctx, v, len)
+        string && writeval(ctx, QUOTE)
         ctx.state[lastindex(ctx.state)] = MINIFY_ARRAY
     end
     return 1
 end
 
-@callback null(ctx::Minifier) = addval(ctx, NULL)
-@callback boolean(ctx::Minifier, v::Bool) = addval(ctx, v)
-@callback number(ctx::Minifier, v::Ptr{UInt8}, len::Int) = addval(ctx, v, len)
-@callback string(ctx::Minifier, v::Ptr{UInt8}, len::Int) = addval(ctx, v, len; string=true)
-@callback function map_start(ctx::Minifier)
-    last(ctx.state) in (MINIFY_ARRAY, MINIFY_MAP_KEY) && write(ctx, COMMA)
-    write(ctx, OPEN_BRACE)
+@yajl null(ctx::Minifier) = addval(ctx, NULL)
+@yajl boolean(ctx::Minifier, v::Bool) = addval(ctx, v)
+@yajl number(ctx::Minifier, v::Ptr{UInt8}, len::Int) = addval(ctx, v, len)
+@yajl string(ctx::Minifier, v::Ptr{UInt8}, len::Int) = addval(ctx, v, len; string=true)
+@yajl function map_start(ctx::Minifier)
+    last(ctx.state) in (MINIFY_ARRAY, MINIFY_MAP_KEY) && writeval(ctx, COMMA)
+    writeval(ctx, OPEN_BRACE)
     push!(ctx.state, MINIFY_MAP_KEY_FIRST)
     return 1
 end
-@callback map_key(ctx::Minifier, v::Ptr{UInt8}, len::Int) = addval(ctx, v, len)
-@callback function map_end(ctx::Minifier)
-    write(ctx, CLOSE_BRACE)
+@yajl map_key(ctx::Minifier, v::Ptr{UInt8}, len::Int) = addval(ctx, v, len)
+@yajl function map_end(ctx::Minifier)
+    writeval(ctx, CLOSE_BRACE)
     pop!(ctx.state)
 
     state = last(ctx.state)
@@ -100,14 +100,14 @@ end
 
     return 1
 end
-@callback function array_start(ctx::Minifier)
-    last(ctx.state) in (MINIFY_ARRAY, MINIFY_MAP_KEY) && write(ctx, COMMA)
-    write(ctx, OPEN_BRACKET)
+@yajl function array_start(ctx::Minifier)
+    last(ctx.state) in (MINIFY_ARRAY, MINIFY_MAP_KEY) && writeval(ctx, COMMA)
+    writeval(ctx, OPEN_BRACKET)
     push!(ctx.state, MINIFY_ARRAY_FIRST)
     return 1
 end
-@callback function array_end(ctx::Minifier)
-    write(ctx, CLOSE_BRACKET)
+@yajl function array_end(ctx::Minifier)
+    writeval(ctx, CLOSE_BRACKET)
     pop!(ctx.state)
 
     state = last(ctx.state)
